e246bd43b460884494536c2795a8cd8027420d14
marinodilauro <m.dilauro90@gmail.com>
2024-04-12 15:58:20 +0200

    Add text with bad word replaced on screen

--- server.php
+++ server.php
@@ -17,7 +17,14 @@ var_dump($_GET);
 <section class="text">
   <h2>Text</h2>
   <p><?php echo $_GET["text"]?></p>
-  <span><?php echo strlen($_GET["text"])?></span>
+  <span>Characters in text: <?php echo strlen($_GET["text"])?></span>
+</section>
+
+<section class="text_with_badword_replaced">
+  <h2>Text with badword replaced</h2>
+  <span>Badword to replace: <?php echo $_GET["badWord"]?></span>
+  <p><?php echo str_replace($_GET["badWord"], "***", $_GET["text"])?></p>
+  <span>Characters in text: <?php echo strlen($_GET["text"])?></span>
 </section>
 
 
